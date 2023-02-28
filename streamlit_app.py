f765093d511d33015ed0690e63176a12a57112a5
Sandhya-Chippagiri <126556124+Sandhya-Chippagiri@users.noreply.github.com>
2023-02-28 14:09:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,7 @@ import streamlit
 
 streamlit.title('My parents New Healthy Diner')
 
-streamlit.title('break fast menu')
+streamlit.header('Breakfast menu')
+streamlit.text('omega 3 and Blueberry oatmeal')
+streamlit.text('Kaale, Spinach and rocket smoothie')
+streamlit.text('Hard boiled free range egg')
